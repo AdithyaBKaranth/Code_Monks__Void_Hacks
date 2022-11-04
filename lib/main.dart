1e3dcb131071541594d94f9971cf149cef009a3d
Rohan Singh R <rohansingh8861@gmail.com>
2022-11-04 17:44:47 +0530

    flutter app build

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/signin.dart';
+
+void main() => runApp(Waste());
+
+class Waste extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(55, 0, 0, 200),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to Dumpy!',
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 25,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 5,
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset('assets/images/dumpy.png')),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 20,
+            ),
+            Text(
+              'Let us contribute together towards the sanity of our country',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => Sign()));
+                },
+                child: Text('Contribute ->'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.red),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
