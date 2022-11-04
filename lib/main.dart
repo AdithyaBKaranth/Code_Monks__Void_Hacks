44e02f4d16f8278ff84b30f9c022df511f51d3ad
iJai007 <117366317+iJai007@users.noreply.github.com>
2022-11-04 19:39:18 +0530

    changed files structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/signin.dart';
+import 'package:flutter_application_1/sign/signin.dart';
 
 void main() => runApp(Waste());
 
